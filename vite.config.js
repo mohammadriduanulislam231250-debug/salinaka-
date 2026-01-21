d55c42cce9e49bdb51894d39b2fd78f40787292e
mohammadriduanulislam231250-debug <mohammadriduanulislam231250@gmail.com>
2026-01-21 21:04:09 +0600

    first build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  base: '/ecommerce-react-v2/', // 🔴 খুব গুরুত্বপূর্ণ
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
